533d473d252c525b6604fbacae2087dfae8156d3
Tomasz Mroczko <tomekmr2001@gmail.com>
2024-10-10 12:48:56 +0200

    chore: remove keycloak client, users will be handled via SPI on keycloak server side

--- src/main/java/com/example/spotspeak/SpotspeakApplication.java
+++ src/main/java/com/example/spotspeak/SpotspeakApplication.java
@@ -2,12 +2,8 @@ package com.example.spotspeak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-import com.example.spotspeak.service.KeycloakConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(KeycloakConfigurationProperties.class)
 public class SpotspeakApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpotspeakApplication.class, args);
---
+++ src/main/java/com/example/spotspeak/entity/UserInfo.java
@@ -0,0 +1,34 @@
+package com.example.spotspeak.entity;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "user_info")
+public class UserInfo {
+
+	@Id
+	@Column(name = "user_id")
+	private UUID user_id;
+
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
+
+	@Column(name = "last_name", nullable = false)
+	private String lastName;
+
+	@Column(name = "username", nullable = false, unique = true)
+	private String username;
+}
