b6b788edce83e84e53a578d208d603011d679bb7
Tomasz Mroczko <tomekmr2001@gmail.com>
2024-10-10 12:48:56 +0200

    chore: remove keycloak client, users will be handled via SPI on keycloak server side

--- src/main/java/com/example/spotspeak/controller/TraceController.java
+++ src/main/java/com/example/spotspeak/controller/TraceController.java
@@ -2,14 +2,12 @@ package com.example.spotspeak.controller;
 
 import java.util.List;
 
-import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.spotspeak.entity.Trace;
-import com.example.spotspeak.service.KeycloakAdminClient;
 import com.example.spotspeak.service.TraceService;
 
 @RestController
@@ -17,23 +15,14 @@ import com.example.spotspeak.service.TraceService;
 public class TraceController {
 
 	private TraceService traceService;
-	private KeycloakAdminClient keycloakAdminClient;
 
-	public TraceController(
-			TraceService traceService,
-			KeycloakAdminClient keycloakAdminClient) {
+	public TraceController(TraceService traceService) {
 		this.traceService = traceService;
-		this.keycloakAdminClient = keycloakAdminClient;
 	}
 
 	@GetMapping
 	public ResponseEntity<List<Trace>> getAllTraces() {
 		return ResponseEntity.ok(traceService.getAllTraces());
-	}
-
-	@GetMapping("/users")
-	public ResponseEntity<List<UserRepresentation>> getUsers() {
-		return ResponseEntity.ok(keycloakAdminClient.getUsers());
 	}
 
 }
