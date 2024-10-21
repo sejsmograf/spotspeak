aa2e6559b50f321758453cbc99dda02ebf7e1c2b
wojtek123o <wojtekwejcht@gmail.com>
2024-10-21 22:35:20 +0200

    feat: add file upload support to createTrace and refactor traceId handling

--- src/main/java/com/example/spotspeak/service/TraceService.java
+++ src/main/java/com/example/spotspeak/service/TraceService.java
@@ -12,6 +12,7 @@ import com.example.spotspeak.repository.TraceRepository;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
 import org.locationtech.jts.geom.Coordinate;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class TraceService {
@@ -20,13 +21,15 @@ public class TraceService {
 	private GeometryFactory geometryFactory;
 	private ResourceService resourceService;
 	private TraceTagService traceTagService;
+	private UserProfileService userProfileService;
 	private S3Service s3Service;
 
-	public TraceService(TraceRepository traceRepository, ResourceService resourceService, TraceTagService traceTagService, S3Service s3Service) {
+	public TraceService(TraceRepository traceRepository, ResourceService resourceService, TraceTagService traceTagService, UserProfileService userProfileService, S3Service s3Service) {
 		this.traceRepository = traceRepository;
 		this.geometryFactory = new GeometryFactory();
 		this.resourceService = resourceService;
 		this.traceTagService = traceTagService;
+		this.userProfileService = userProfileService;
 		this.s3Service = s3Service;
 	}
 
@@ -34,27 +37,17 @@ public class TraceService {
 		return (List<Trace>) traceRepository.findAll();
 	}
 
-	public Trace findTraceById(Long traceId) {
-		return traceRepository.findById(traceId).orElseThrow(
-				() -> {
-					throw new TraceNotFoundException("Could not find the trace");
-				});
-	}
-
-
-	public Trace createTrace(TraceUploadDTO traceUploadDTO, User author) {
+	@Transactional
+	public Trace createTrace(String userId, MultipartFile file, TraceUploadDTO traceUploadDTO) {
 		Point point = geometryFactory.createPoint(new Coordinate(traceUploadDTO.longitude(), traceUploadDTO.latitude()));
 
-		Resource resource = resourceService.createAndSaveResource(
-				traceUploadDTO.keyName(),
-				traceUploadDTO.fileName(),
-				traceUploadDTO.fileType()
-		);
+		User user = userProfileService.findByIdOrThrow(userId);
+		Resource resource = resourceService.uploadTraceResource(userId, file);
 
 		Trace trace = Trace.builder()
 				.location(point)
 				.description(traceUploadDTO.description())
-				.author(author)
+				.author(user)
 				.resource(resource)
 				.isActive(true)
 				.build();
@@ -63,30 +56,42 @@ public class TraceService {
 	}
 
 	@Transactional
-	public TraceDownloadDTO getTraceInfo(Long traceId) {
-		Trace trace = findTraceById(traceId);
+	public TraceDownloadDTO getTraceInfo(String userId, String traceId) {
+		Trace trace = findByIdOrThrow(traceId);
+		userProfileService.findByIdOrThrow(userId); //maybe not necessary
+
 		String keyName = trace.getResource().getS3Key();
 
 		String presignedUrl = s3Service.generatePresignedDownloadUrl(keyName);
-
-		Point location = trace.getLocation();
-		Double latitude = location.getY();
-		Double longitude = location.getX();
-
-		List<Tag> tags = traceTagService.getTagsForTrace(traceId);
-		List<Comment> comments = trace.getComments();
 
         return new TraceDownloadDTO(
 			trace.getId(),
 			presignedUrl,
 			trace.getDescription(),
-			comments,
-			tags,
-			latitude,
-			longitude,
-			trace.getAuthor().getFirstName(),
-			trace.getAuthor().getLastName(),
-			trace.getAuthor().getUsername()
+			trace.getComments(),
+			traceTagService.getTagsForTrace(trace.getId()),
+			trace.getLocation().getX(),
+			trace.getLocation().getY(),
+			trace.getAuthor()
 		);
+	}
+
+	private Trace findByIdOrThrow(Long traceId) {
+		return traceRepository.findById(traceId).orElseThrow(
+				() -> new TraceNotFoundException("Could not find trace with id: " + traceId)
+		);
+	}
+
+	private Long traceIdToLong(String traceIdString) {
+		try {
+			return Long.valueOf(traceIdString);
+		} catch (NumberFormatException e) {
+			throw new TraceNotFoundException("Invalid traceId format: " + traceIdString);
+		}
+	}
+
+	public Trace findByIdOrThrow(String traceIdString) {
+		Long traceId = traceIdToLong(traceIdString);
+		return findByIdOrThrow(traceId);
 	}
 }
