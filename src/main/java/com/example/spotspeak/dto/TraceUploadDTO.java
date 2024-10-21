ebcdae71fb3f628e665c0adc28e59272765a1bed
wojtek123o <wojtekwejcht@gmail.com>
2024-10-21 22:36:08 +0200

    refactor: simplify TraceUploadDTO by removing file-related fields

--- src/main/java/com/example/spotspeak/dto/TraceUploadDTO.java
+++ src/main/java/com/example/spotspeak/dto/TraceUploadDTO.java
@@ -3,8 +3,5 @@ package com.example.spotspeak.dto;
 public record TraceUploadDTO(
         Double latitude,
         Double longitude,
-        String description,
-        String keyName,
-        String fileName,
-        String fileType) {
+        String description) {
 }
