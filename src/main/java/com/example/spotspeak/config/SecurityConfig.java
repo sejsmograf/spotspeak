53f41f029dd1d4c5769b2e77cd7a004a8bf09f24
Tomasz Mroczko <tomekmr2001@gmail.com>
2024-10-22 19:59:10 +0200

    feat: add OpenApi documentation

--- src/main/java/com/example/spotspeak/config/SecurityConfig.java
+++ src/main/java/com/example/spotspeak/config/SecurityConfig.java
@@ -15,7 +15,9 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(
-                authorize -> authorize.anyRequest().authenticated());
+                authorize -> authorize
+                        .requestMatchers("/api/docs/**", "/swagger-ui/**").permitAll()
+                        .anyRequest().authenticated());
 
         http.oauth2ResourceServer(
                 oauth2 -> oauth2.jwt(Customizer.withDefaults()));
