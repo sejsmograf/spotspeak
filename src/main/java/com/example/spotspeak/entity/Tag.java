4895470c30a8721a78c2c8365604126bf73ca715
Tomasz Mroczko <tomekmr2001@gmail.com>
2024-10-26 18:16:38 +0200

    feat: fix deletion on tags

--- src/main/java/com/example/spotspeak/entity/Tag.java
+++ src/main/java/com/example/spotspeak/entity/Tag.java
@@ -2,18 +2,25 @@ package com.example.spotspeak.entity;
 
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Table(name = "tags")
 public class Tag {
@@ -22,9 +29,10 @@ public class Tag {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
-	@Column(nullable = false)
+	@Column(nullable = false, unique = true)
 	private String name;
 
-	@ManyToMany(mappedBy = "tags")
+	@JsonIgnore
+	@ManyToMany(mappedBy = "tags", fetch = FetchType.LAZY)
 	Set<Trace> traces;
 }
