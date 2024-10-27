823c7e5862e3ad46a49e8980b3e1392b14a3fce2
wojtek123o <wojtekwejcht@gmail.com>
2024-10-27 18:44:47 +0100

    feat: added methods to FriendRequestRepository for querying friend requests

--- src/main/java/com/example/spotspeak/repository/FriendRequestRepository.java
+++ src/main/java/com/example/spotspeak/repository/FriendRequestRepository.java
@@ -1,8 +1,17 @@
 package com.example.spotspeak.repository;
 
 import com.example.spotspeak.entity.FriendRequest;
+import com.example.spotspeak.entity.User;
+import com.example.spotspeak.entity.enumeration.EFriendRequestStatus;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface FriendRequestRepository extends CrudRepository<FriendRequest, Long> {
 
+    boolean existsBySenderAndReceiverAndStatus(User sender, User receiver, EFriendRequestStatus status);
+
+    List<FriendRequest> findBySenderAndStatus(User sender, EFriendRequestStatus status);
+
+    List<FriendRequest> findByReceiverAndStatus(User receiver, EFriendRequestStatus status);
 }
