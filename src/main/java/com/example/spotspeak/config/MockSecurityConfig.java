3b359772b2486b0587da83e45b4813214e4c5776
Tomasz Mroczko <tomekmr2001@gmail.com>
2024-10-29 11:53:18 +0100

    feat: move mockuserid to env variable. Disable caching for user-uploads in order to serve them immediately

--- src/main/java/com/example/spotspeak/config/MockSecurityConfig.java
+++ src/main/java/com/example/spotspeak/config/MockSecurityConfig.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -38,14 +39,17 @@ public class MockSecurityConfig {
         return http.build();
     }
 
+    @Value("${mock.user.id}")
+    private String mockUserId;
+
     @Bean
     OncePerRequestFilter mockJwtAuthenticationFilter() {
         return new OncePerRequestFilter() {
             @Override
             protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                     FilterChain filterChain) throws ServletException, IOException {
-                String userId = "1f6e9b7d-8c47-4b68-8e80-27e5a9c73917";
-                Jwt mockJwt = createMockJwt(userId);
+
+                Jwt mockJwt = createMockJwt(mockUserId);
                 AbstractAuthenticationToken auth = new UsernamePasswordAuthenticationToken(mockJwt, null);
                 SecurityContextHolder.getContext().setAuthentication(auth);
                 filterChain.doFilter(request, response);
