7bf1250c8413cc51e1d1bd6e37362fc64883a4f9
wojtek123o <wojtekwejcht@gmail.com>
2024-10-30 18:42:50 +0100

    feat: add mentions relationship in Comment entity

--- src/main/java/com/example/spotspeak/entity/Comment.java
+++ src/main/java/com/example/spotspeak/entity/Comment.java
@@ -5,16 +5,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.CascadeType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @Entity
@@ -38,6 +41,9 @@ public class Comment {
     @Column(nullable = false)
     private String content;
 
-    @CreatedDate
+    @OneToMany(mappedBy = "comment", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CommentMention> mentions;
+
+    @CreationTimestamp
     private LocalDateTime createdAt;
 }
