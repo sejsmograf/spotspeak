5a4e8ef9b1479f6c783c3cd19624463416748804
wojtek123o <wojtekwejcht@gmail.com>
2024-10-30 18:48:42 +0100

    feat: add batch username retrieval methods in UserRepository and UserService

--- src/main/java/com/example/spotspeak/service/UserService.java
+++ src/main/java/com/example/spotspeak/service/UserService.java
@@ -118,4 +118,11 @@ public class UserService {
 		UUID convertedId = userIdToUUID(userIdString);
 		return findByIdOrThrow(convertedId);
 	}
+
+	public List<User> findUsersByUsernames(List<String> usernames) {
+		if (usernames == null || usernames.isEmpty()) {
+			return List.of();
+		}
+		return userRepostitory.findByUsernameIn(usernames);
+	}
 }
