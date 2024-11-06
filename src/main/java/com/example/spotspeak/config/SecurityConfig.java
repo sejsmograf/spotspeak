c6aa762ff6a673b00242b99e021c271cbab8939f
Tomasz Mroczko <tomekmr2001@gmail.com>
2024-11-06 11:42:47 +0100

    chore: small improvements to securityconfig

--- src/main/java/com/example/spotspeak/config/SecurityConfig.java
+++ src/main/java/com/example/spotspeak/config/SecurityConfig.java
@@ -2,9 +2,11 @@ package com.example.spotspeak.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,13 +16,25 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(csrf -> csrf.disable());
+        // Stateless API doesn't need CSRF
+        http.csrf(
+                csrf -> csrf.disable());
 
+        // JWT based authentication doesnt need sessions
+        http.sessionManagement(
+                session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        // Permit access to Swagger UI and API documentation
         http.authorizeHttpRequests(
                 authorize -> authorize
                         .requestMatchers("/api/docs/**", "/swagger-ui/**").permitAll()
                         .anyRequest().authenticated());
 
+        // Disable form login
+        http.formLogin(
+                formLogin -> formLogin.disable());
+
+        // Enable OAuth2 resource server
         http.oauth2ResourceServer(
                 oauth2 -> oauth2.jwt(Customizer.withDefaults()));
 
