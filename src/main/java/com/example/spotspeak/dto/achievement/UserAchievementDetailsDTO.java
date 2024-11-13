de4413922d968565a1843b34795e4b8241cac150
wojtek123o <wojtekwejcht@gmail.com>
2024-11-13 23:40:00 +0100

    feat: added new UserAchievementDetailsDTO

---
+++ src/main/java/com/example/spotspeak/dto/achievement/UserAchievementDetailsDTO.java
@@ -0,0 +1,15 @@
+package com.example.spotspeak.dto.achievement;
+
+import java.time.LocalDateTime;
+
+public record UserAchievementDetailsDTO(
+    Long userAchievementId,
+    String achievementName,
+    String achievementDescription,
+    String resourceAccessUrl,
+    Integer points,
+    Integer requiredQuantity,
+    Integer quantityProgress,
+    Integer currentStreak,
+    LocalDateTime completedAt
+) {}
