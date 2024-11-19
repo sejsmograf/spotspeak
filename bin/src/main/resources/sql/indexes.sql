a3047be8d8728f77c35ebec28d81532c79e9cf63
Tomasz Mroczko <tomekmr2001@gmail.com>
2024-11-19 19:48:07 +0100

    remove bin directory

--- bin/src/main/resources/sql/indexes.sql
+++
@@ -1,9 +0,0 @@
--- Trace indexes
-CREATE INDEX IF NOT EXISTS idx_trace_location ON traces USING GIST (location);
-CREATE INDEX IF NOT EXISTS idx_trace_active ON traces (is_active);
-CREATE INDEX IF NOT EXISTS idx_trace_author ON traces (author_id);
-CREATE INDEX IF NOT EXISTS idx_trace_event ON traces (event_id);
-
--- Comment indexes
-CREATE INDEX IF NOT EXISTS idx_comment_author ON comments (author_id);
-CREATE INDEX IF NOT EXISTS idx_comment_trace ON comments (trace_id);
