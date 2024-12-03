909363866d609bc02c4ddd8e56363283bc2859d1
Tomasz Mroczko <tomekmr2001@gmail.com>
2024-12-03 16:55:31 +0100

    bugfix: timezone

--- src/main/java/com/example/spotspeak/SpotspeakApplication.java
+++ src/main/java/com/example/spotspeak/SpotspeakApplication.java
@@ -1,5 +1,7 @@
 package com.example.spotspeak;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -7,11 +9,18 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.example.spotspeak.config.KeycloakClientConfiguration;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 @EnableConfigurationProperties(KeycloakClientConfiguration.class)
 @EnableScheduling
 public class SpotspeakApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpotspeakApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Warsaw"));
     }
 }
