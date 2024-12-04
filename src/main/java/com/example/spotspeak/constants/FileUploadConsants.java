7fefcbd3f9c8ccd8b7c700593cfea735c237aa14
Tomasz Mroczko <tomekmr2001@gmail.com>
2024-12-04 09:15:05 +0100

    chore: increase max trace media upload size

--- src/main/java/com/example/spotspeak/constants/FileUploadConsants.java
+++ src/main/java/com/example/spotspeak/constants/FileUploadConsants.java
@@ -2,7 +2,7 @@ package com.example.spotspeak.constants;
 
 public class FileUploadConsants {
     public static final long PROFILE_PICTURE_MAX_SIZE = 1024 * 1024 * 10;
-    public static final long TRACE_MEDIA_MAX_SIZE = 1024 * 1024 * 20;
+    public static final long TRACE_MEDIA_MAX_SIZE = 1024 * 1024 * 50;
     public static final long ACHIEVEMENT_ICON_MAX_SIZE = 1024 * 1024 * 5;
 
     public static final String[] PROFILE_PICTURE_ALLOWED_TYPES = {
